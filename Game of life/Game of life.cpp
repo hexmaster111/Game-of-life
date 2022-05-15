77fb89de30f3599a245d17901ebfdf7dc6e7ac40
Hailey <Hexmaster111@gmail.com>
2022-05-14 21:10:02 -0500

    now with cpp

--- Game of life/Game of life.cpp
+++ Game of life/Game of life.cpp
@@ -2,7 +2,11 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
 #include <Windows.h>
+
+using namespace std;
 
 void clear_screen(char fill = ' ') {
 	COORD tl = { 0,0 };
@@ -16,29 +20,21 @@ void clear_screen(char fill = ' ') {
 }
 
 
+
 const char* spacer = "-----------------------------------";
 
-int cell[8][8] = {
-	0,0,0,0,0,0,0,0,
-	0,0,0,1,0,0,0,0,
-	0,0,0,1,0,0,0,0,
-	0,0,0,1,0,0,0,0,
-	0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,1,
-	0,0,0,0,0,0,0,1,
-	0,0,0,0,0,0,0,1 };
+std::vector<std::vector<bool>> c_cell =
+{
+	{0,0,0,0,0},
+	{0,0,1,0,0},
+	{0,0,1,0,0},
+	{0,0,1,0,0},
+	{0,0,0,0,0},
 
-int out[8][8] = {
-	0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0 };
+}, c_out;
 
-int count_friends(int _c[8][8], int x, int y) {
+
+int count_friends(vector<vector<bool>> _c, int x, int y) {
 
 	/*
 	sxx
@@ -48,40 +44,65 @@ int count_friends(int _c[8][8], int x, int y) {
 
 	int _o = 0;
 
-	if (_c[y][x + 1] == 1) {
-		_o++;
-	}
-	if (_c[y + 1][x] == 1) {
-		_o++;
-	}
-	if (_c[y - 1][x] == 1) {
-		_o++;
-	}
-	if (_c[y][x - 1] == 1) {
-		_o++;
-	}
-	if (_c[y + 1][x + 1] == 1) {
-		_o++;
-	}
-	if (_c[y + 1][x - 1] == 1) {
-		_o++;
-	}
-	if (_c[y - 1][x + 1] == 1) {
-		_o++;
-	}
-	if (_c[y - 1][x - 1] == 1) {
-		_o++;
+	//y+0, x+1
+
+	if (x != 0) {
+
+		if (_c.at(y).at(x - 1)) {
+			_o++;
+		}
 	}
 
+	if (y != 0) {
+		if (_c.at(y - 1).at(x)) {
+			_o++;
+		}
+	}
+
+	if (y + 1 != _c.size()) {
+		if (_c.at(y + 1).at(x)) {
+			_o++;
+		}
+	}
+
+	if (x + 1 != _c.at(y).size()) {
+		if (_c.at(y).at(x + 1)) {
+			_o++;
+		}
+	}
+
+	if (x != 0 and y != 0) {
+		if (_c.at(y - 1).at(x - 1)) {
+			_o++;
+		}
+	}
+
+	if (x != 0 and y + 1 != _c.size()) {
+		if (_c.at(y + 1).at(x - 1)) {
+			_o++;
+		}
+	}
+
+	if (x + 1 != _c.at(y).size() and y != 0) {
+		if (_c.at(y - 1).at(x + 1)) {
+			_o++;
+		}
+	}
+
+	if (x + 1 != _c.at(y).size() and y + 1 != _c.size()) {
+		if (_c.at(y + 1).at(x + 1)) {
+			_o++;
+		}
+	}
 
 	return _o;
 }
 
-void show_cell(int _c[8][8])
+void show_cell(vector<vector<bool>> _c)
 {
-	for (size_t i = 0; i < sizeof(_c[0]) / sizeof(_c[0][0]); i++)
+	for (size_t i = 0; i < _c.size(); i++)
 	{
-		for (size_t m = 0; m < 8; m++)
+		for (size_t m = 0; m < _c[i].size(); m++)
 		{
 			std::cout << _c[i][m];
 		}
@@ -97,11 +118,12 @@ void game_logic() {
 
 	int _around_alive = 0;
 
-	for (size_t m = 0; m < 8; m++)
+
+	for (size_t m = 0; m < c_cell.size() - 1; m++)
 	{
-		for (size_t i = 0; i < 8; i++)
+		for (size_t i = 0; i < c_cell[i].size() - 1; i++)
 		{
-			int _around_alive = count_friends(cell, i, m);
+			int _around_alive = count_friends(c_cell, i, m);
 
 			// if a cell has zero alive nabors it dies
 			// if a cell has 1 alive nabors it dies
@@ -111,30 +133,30 @@ void game_logic() {
 			switch (_around_alive)
 			{
 			case 0:
-				out[m][i] = 0;
+				c_out.at(m).at(i) = false;
 				break;
 
 			case 1:
-				out[m][i] = 0;
+				c_out[m][i] = 0;
 				break;
 
 			case 2:
-				if (cell[m][i] == 1)
+				if (c_cell[m][i] == 1)
 				{
-					out[m][i] = 1;
+					c_out[m][i] = 1;
 				}
 				else
 				{
-					out[m][i] = 0;
+					c_out[m][i] = 0;
 				}
 				break;
 
 			case 3:
-				out[m][i] = 1;
+				c_out[m][i] = 1;
 				break;
 
 			default: // This will be four or more
-				out[m][i] = 0;
+				c_out[m][i] = 0;
 				break;
 			}
 			std::cout << _around_alive;
@@ -146,21 +168,22 @@ void game_logic() {
 
 int main() {
 	std::cout << "Conways Game of Life\n";
+	c_out.assign(c_cell.begin(), c_cell.end());
 	std::cout << spacer << "\n";
-	show_cell(cell);
+	show_cell(c_cell);
 	std::cout << spacer << "\n";
 	game_logic();
 	std::cout << spacer << "\n";
-	show_cell(out);
+	show_cell(c_out);
 
-	memcpy(cell, out, sizeof(cell));
+
 
 	while (1) {
 		clear_screen();
 		game_logic();
 		std::cout << spacer << "\n";
-		show_cell(out);
-		memcpy(cell, out, sizeof(cell));
+		show_cell(c_out);
+		c_cell.assign(c_out.begin(), c_out.end());
 		Sleep(500);
 	}
 	return 0;
